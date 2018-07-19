77c231bb14b3c4742871f18aa7e8d3cefffe6df4
Sean Christopher Febo <scf@techie.com>
2018-07-19 20:15:54 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,35 @@
+let store = {drivers: [], passengers: [], trips: []};
+let driverId = 0; 
+let passengerId = 0;
+let tripId = 0;
+
+class Driver { 
+  constructor(name) { 
+    this. name = name;
+    this.id = driverId++; 
+    store.drivers.push(this);
+  }
+  
+}
+
+class Passenger { 
+  constructor(name) { 
+    this.name = name;
+    this.id = passengerId++;
+    store.passengers.push(this);
+  }
+}
+
+class Trip { 
+  constructor(driver, passenger) { 
+    this.driver = driver;
+    this.passenger = passenger;
+    this.id = tripId++; 
+    this.driverId = driverId;
+    this.passengerId = passengerId;
+    store.trips.push(this);
+  }
+  driver() { 
+    return store.drivers.filter(driver => {
+      return driver.id === 
+    })
