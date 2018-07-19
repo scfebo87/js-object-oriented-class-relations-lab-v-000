a2993aff46fdb72205a9adb3b6b60c92d19a9e08
Sean Christopher Febo <scf@techie.com>
2018-07-19 20:47:06 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,61 @@
+let store = {drivers: [], passengers: [], trips: []};
+let driverId = 0; 
+let passengerId = 0;
+let tripId = 0;
+
+class Driver { 
+  constructor(name) { 
+    this.name = name;
+    this.id = ++driverId; 
+    store.drivers.push(this);
+  }
+  trips() { 
+    return store.trips.filter(trip => { 
+      return trip.driverId === this.id;
+    });
+  }
+  
+  passengers() { 
+    return store.passengers.filter(passenger => { 
+      return this.trips().map(trip => trip.passengerId);
+    });
+  }
+}
+
+class Passenger { 
+  constructor(name) { 
+    this.name = name;
+    this.id = ++passengerId;
+    store.passengers.push(this);
+  }
+  trips() { 
+    return store.trips.filter(trip => { 
+      return trip.passengerId === this.id;
+    });
+  }
+  drivers() { 
+    return store.drivers.filter(driver => { 
+      return this.trips().map(trip => trip.driverId);
+    });
+  }
+}
+
+class Trip { 
+  constructor(driver, passenger) { 
+    this.id = ++tripId; 
+    this.driverId = driver.id;
+    this.passengerId = passenger.id;
+    store.trips.push(this);
+  }
+  driver() { 
+    return store.drivers.find(driver => {
+      return driver.id === this.driverId;
+    });
+  }
+  
+  passenger() { 
+    return store.passengers.find(passenger => {
+      return passenger.id === this.passengerId;
+    });
+  }
+}
